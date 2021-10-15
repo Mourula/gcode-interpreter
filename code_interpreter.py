fbf3f3c586f08457a2b2d50ee0099c7083244abc
Tiina Karhukivi <tiina.karhukivi@gmail.com>
2021-10-15 21:01:39 +0300

    Clean code, add unit tests
    Cleaned up code by adding more modules.
    Added unit tests with mocking.

---
+++ code_interpreter.py
@@ -0,0 +1,89 @@
+"""
+G-Code interpreter methods.
+Author: Tiina Karhukivi
+"""
+
+
+def codeInterpreter(itemList, client):
+    """ Interpret G-Code Lines
+        Args:
+        itemList (list[str]): List of G-Codes from a row
+        client (MachineClient): Client for API calls
+    """
+
+    # Check special case: Are X, Y and Z all present in a row?
+    xFound = False
+    yFound = False
+    zFound = False
+    for item in itemList:
+        if item.find("X") != -1:
+            xFound = True
+        elif item.find("Y") != -1:
+            yFound = True
+        elif item.find("Z") != -1:
+            zFound = True
+
+    xyzFound = (xFound and yFound and zFound)
+
+    for item in itemList:
+        if (len(item) < 2):
+            continue
+
+        initial = item[0]
+        value = item[1:]
+
+        if initial == "X":
+            if xyzFound:
+                xVal = value
+            else:
+                client.move_x(float(value))
+        elif initial == "Y":
+            if xyzFound:
+                yVal = value
+            else:
+                client.move_y(float(value))
+        elif initial == "Z":
+            if xyzFound:
+                client.move(float(xVal), float(yVal), float(value))
+            else:
+                client.move_z(float(value))
+        elif initial == "F":
+            client.set_feed_rate(float(value))
+        elif initial == "S":
+            client.set_spindle_speed(int(value))
+        elif initial == "M":
+            if value == "8" or value == "08":
+                client.coolant_on()
+            elif value == "9" or value == "09":
+                client.coolant_off()
+            elif value == "30":
+                client.home()
+        elif initial == "T":
+            client.change_tool(value)
+
+
+def isCodeLine(line):
+    """ Check if line includes G-Code commands
+        Args:
+        line (str): Row from a file
+    """
+
+    cleanLine = line.strip()
+
+    # Empty Row
+    if len(cleanLine) == 0:
+        return False
+
+    # Begin/End Mark
+    if cleanLine[0] == "%":
+        return False
+
+    # Program Number
+    if cleanLine[0] == "O":
+        return False
+
+    # Comment Line
+    if cleanLine[0] == "(":
+        return False
+
+    return True
---
+++ unittests.py
@@ -0,0 +1,59 @@
+import unittest
+from unittest.mock import MagicMock
+from code_interpreter import isCodeLine, codeInterpreter
+from machine_client import MachineClient
+
+
+class TestInterpreterMethods(unittest.TestCase):
+
+    def test_skippingLines(self):
+        self.assertFalse(isCodeLine("%"))
+        self.assertFalse(isCodeLine("(Here I'm commenting the G-Code)"))
+        self.assertFalse(isCodeLine("O12345"))
+        self.assertFalse(isCodeLine(""))
+
+    def test_greatLines(self):
+        self.assertTrue("N4 T01 M06")
+
+    def test_homeIsCalled(self):
+        thing = MachineClient()
+        thing.home = MagicMock()
+        itemList = ["N01", "G20", "M30"]
+        codeInterpreter(itemList, thing)
+
+        self.assertTrue(thing.home.called)
+
+    def test_xyzIsCalled(self):
+        thing = MachineClient()
+        thing.move = MagicMock()
+        thing.move_x = MagicMock()
+        thing.move_y = MagicMock()
+        thing.move_z = MagicMock()
+        itemList = ["N01", "X20.", "Y30.5", "Z22"]
+        codeInterpreter(itemList, thing)
+
+        self.assertTrue(thing.move.called)
+        self.assertFalse(thing.move_x.called)
+        self.assertFalse(thing.move_y.called)
+        self.assertFalse(thing.move_z.called)
+        thing.move.assert_called_once_with(20.0, 30.5, 22.0)
+
+    def test_xzIsCalled(self):
+        thing = MachineClient()
+        thing.move = MagicMock()
+        thing.move_x = MagicMock()
+        thing.move_y = MagicMock()
+        thing.move_z = MagicMock()
+        itemList = ["N01", "X.2", "Z-30.", "G22"]
+        codeInterpreter(itemList, thing)
+
+        self.assertFalse(thing.move.called)
+        self.assertTrue(thing.move_x.called)
+        self.assertFalse(thing.move_y.called)
+        self.assertTrue(thing.move_z.called)
+        thing.move_x.assert_called_once_with(0.2)
+        thing.move_z.assert_called_once_with(-30.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
